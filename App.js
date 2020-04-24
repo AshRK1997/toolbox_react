5800478e18a10c6689705c4013d6e8912a2a5adc
Ashwin Ram Kumar <reachashrk@gmail.com>
2020-04-24 09:54:45 +0530

    commited

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 export default class App extends Component {
   render() {
     return (
-      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      <ScrollView style=styles.scrollView>
       <SafeAreaView style={styles.safeAreaView}>
 
         <View style={styles.container}>
@@ -57,7 +57,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   scrollView:{
-
+    flexGrow: 1,
     textAlign:'center'
   },
   safeAreaView: {
